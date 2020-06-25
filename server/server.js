92b731ff00e9e792e868f9f7ed56d318ea35c14e
Tye Macon <tyemacon@gmail.com>
2020-06-25 12:42:51 -0500

    automatic update test

--- server/server.js
+++ server/server.js
@@ -1,31 +1,25 @@
 const express = require('express')
 const path = require('path');
 // const db = require('./db');
-const cors = require('cors');
+// const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors);
-app.use(express.static(path.join(__dirname, '../build')));
+// app.use(cors);
+app.use(express.static(path.join(__dirname, '/../build')));
 
-let port = 3000;
+let port = 3030;
 
-// app.get('/', (req, res) => {
-//   res.send('hey there')
-// })
+app.get('/', (req, res) => {
+  res.send('hey there')
+})
 
-// app.get('/getAll', (req, res) => {
-//   console.log('getting all...')
-//   db.findAllGames((err, games) => {
-//     if(err){
-//       throw err
-//     }else{
-//       res.send(games);
-//     }
-//   })
-// })
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../build/index.html"))
+});
 
 app.listen(port, () => {
   console.log(`...Listening on port ${port}...`)
